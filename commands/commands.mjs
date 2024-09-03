b76e66eb04b9477208d80419d41c7221af2e4afa
KoruJuriSan <mr.collejoulian@gmail.com>
2024-09-03 14:28:23 +0200

    style: Reorganizing the `commands.mjs` file

--- commands/commands.mjs
+++ commands/commands.mjs
@@ -1,20 +1,26 @@
+// Import Admin commands
 import createAnnonce from "./admin/createAnnonce.mjs"
+import setChannel from "./admin/SetChannel.mjs"
+
+// Import User commands
 import whoami from "./user/whoami.mjs"
 import ping from "./user/ping.mjs"
 import ipInfo from "./user/ipInfo.mjs"
 import binaryOperation from "./user/binaryOperation.mjs"
 import convertBase from "./user/convertBase.mjs"
 import info from "./user/info.mjs"
-import setChannel from "./admin/SetChannel.mjs"
 
 const commands = [
-    createAnnonce,
+    // User commands
     whoami,
     ping,
     ipInfo,
     binaryOperation,
     convertBase,
     info,
+
+    // Admin commands
+    createAnnonce,
     setChannel
 ]
 
