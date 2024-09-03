dbd45866ab59fa8eaf0e88d17bbbfe961f4a2d35
KoruJuriSan <mr.collejoulian@gmail.com>
2024-09-03 14:49:10 +0200

    ci: moving DB setup in its own file

--- index.mjs
+++ index.mjs
@@ -1,23 +1,15 @@
 import { Client, Events, GatewayIntentBits } from "discord.js"
 import interactionCreate from "./events/interactionCreate.mjs"
 import clientReady from "./events/clientReady.mjs"
-import mongoose from "mongoose"
 import dotenv from "dotenv"
+import mongodbSetup from "./lib/mongodbSetup.mjs"
 
 async function main() {
 
     dotenv.config()
     const token = process.env.TOKEN
 
-    let isDbConnected = false
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/desmet-chan-bot")
-        isDbConnected = true
-        console.log("Mongodb connected on 'desmet-chan-bot' DB")
-    } catch(err) {
-        console.log("Couldn't connect to Mongodb on 'desmet-chan-bot' DB:")
-        console.error(err)
-    }
+    const isDbConnected = mongodbSetup()
 
     const client = new Client({ intents: [
         GatewayIntentBits.Guilds,
